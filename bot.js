b3d92efdc8f92251e2f775ad8721b965dd6507c0
hgdfgxsgsg <43437178+hgdfgxsgsg@users.noreply.github.com>
2018-09-29 10:41:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -635,7 +635,7 @@ client.on('message', async msg => {
 
 client.on('message', message => {
     if(message.content.includes('discord.gg')){
-                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? 🤔   **');
+                                            if(!message.channel.guild) return message.reply('** من صاحب البوت   **');
         if (!message.member.hasPermissions(['ADMINISTRATOR'])){
         message.delete()
     return message.reply(`** No Invite Links |ممنوع نشر السيرفرات:angry: !**`)
@@ -998,13 +998,16 @@ member.guild.owner.send(join)
   
 
 client.on('message', message => {
-        if (message.content.toLowerCase() === prefix + "inv") {
+        if (message.content === "-inv") {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
-        .setTitle(`:small_orange_diamond: Click Here To Invite | أضغط لدعوة البوت :small_orange_diamond: `)
-        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=484518205495443476&permissions=8&scope=bot`)
+        .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
+        .setTitle(`:small_orange_diamond: اضغط هنا `)
+        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=466256531998900246&permissions=8&scope=bot`)
+        .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
        }
+   });
 
 
 
