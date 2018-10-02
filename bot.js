e34aac7ad15cdd1a63e9669c94462ed844738bdc
hgdfgxsgsg <43437178+hgdfgxsgsg@users.noreply.github.com>
2018-10-02 19:46:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1065,47 +1065,15 @@ Servers Counter : __${client.guilds.size}__**`)
   
 
 
-client.on("message", message => {
-
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("name", Rainbow );
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
-
-  if(message.content.startsWith(prefix + "startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("css\nDiscoing...");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`nooooo`);
-  }
-} else
-
-if(message.content.startsWith(prefix + "stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`nooooooo`);
-  }
-}
-
-});
-
-
-
-
-
 client.on("ready", () => {
-    client.user.setPresence({ game: { name: `Rainbow` }, type: 0 });
-    console.log("Disco role bot online! Created by i am toast.");
-});
+     client.guilds.map( guild => {
+         setInterval(function(){
+             guild.roles.find("name", "Rainbow").edit({
+                 color : "RANDOM"
+             });
+         }, 600)
+     });
+ });
 
 
 
